7a92051c080376b58f08e4f6fe8b92d7ca9e2841
Moises Santos <moisesbarbosa23@gmail.com>
2024-11-04 19:48:22 -0300

    chore[backend]: habilitando cors

--- src/main/java/infnet/arquitetura_java_biblioteca/config/WebConfig.java
+++ src/main/java/infnet/arquitetura_java_biblioteca/config/WebConfig.java
@@ -1,4 +1,16 @@
 package infnet.arquitetura_java_biblioteca.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*");
+    }
 }
